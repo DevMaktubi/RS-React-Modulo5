3e9f3cab7f1ada256e51c47d461c5637f143f3cb
devmaktubi <brenoluiz1@gmail.com>
2022-02-22 14:02:07 -0300

    Added Next Auth, such a nice lib

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,26 @@
 import {FaGithub} from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
+import {signIn, signOut, useSession} from 'next-auth/react'
+
 import styles from './styles.module.scss'
 
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
 
-  return isUserLoggedIn ? 
+  const {data: session} = useSession();
+
+  console.log(session)
+
+  return session ? 
     (
       <button
-      className={styles.signInButton}
-      type="button">
+        className={styles.signInButton}
+        type="button"
+        onClick={() => signOut()}
+      >
         <FaGithub color={'#04d361'}/>
-        Dev Maktub
+        {session.user.name}
         <FiX color='#737380' className={styles.closeIcon}/>
       </button>
     )
@@ -21,7 +28,9 @@ export function SignInButton() {
     (
       <button
        className={styles.signInButton}
-       type="button">
+       type="button"
+        onClick={() => signIn('github')}
+      >
         <FaGithub color={'#eba417'}/>
         Sign in with Github
       </button>
